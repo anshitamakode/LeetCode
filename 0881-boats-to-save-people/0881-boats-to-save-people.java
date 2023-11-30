35988531bc10705a3a4743268cad6ea37161ce52
Anshita Makode <53944279+anshitamakode@users.noreply.github.com>
2023-11-30 23:10:09 +0530

    Time: 16 ms (94.39%), Space: 53 MB (62.90%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int cnt = 0;
+        int n = people.length;
+        int i = 0, j = n-1;
+        while(i <= j) {
+            int sum = people[i] + people[j];
+            if(sum <= limit){
+                i++;
+                cnt++;
+            } 
+            else{
+                cnt++;
+            }
+            j--;
+        }
+        return cnt;
+    }
+}
