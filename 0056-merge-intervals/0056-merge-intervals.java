eda570a3054701b544d35c64a9781135b8a1ef98
Anshita Makode <53944279+anshitamakode@users.noreply.github.com>
2023-12-12 22:28:35 +0530

    Time: 10 ms (30.31%), Space: 46.1 MB (15.12%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> answer = new ArrayList<>();
+        int n = intervals.length;
+        if(n != 0){
+            Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+            int start = intervals[0][0];
+            int end = intervals[0][1];
+            for(int[] val: intervals){
+                if(val[0] <= end){
+                    end = Math.max(end, val[1]);
+                } 
+                else{
+                    answer.add(new int[]{start,end});
+                    start = val[0];
+                    end = val[1];
+                }
+            }
+            answer.add(new int[]{start,end}); 
+        }
+        return answer.toArray(new int[0][]); 
+    }
+}
