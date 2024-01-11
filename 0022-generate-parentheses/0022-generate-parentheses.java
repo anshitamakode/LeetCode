bec9fbbafb1fa2c9a3ea51fe83e655a1cf34cd8d
Anshita Makode <53944279+anshitamakode@users.noreply.github.com>
2024-01-11 21:58:08 +0530

    Time: 1 ms (88.72%), Space: 42.9 MB (69.34%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,19 +4,19 @@ class Solution {
         generateOneByOne("", list, n, n);
         return list;
     }
-    public void generateOneByOne(String sublist, List<String> list, int open, int close){
+    public void generateOneByOne(String substr, List<String> list, int open, int close){
         if(open > close){
             return;
         }
+        if(open == 0 && close == 0){
+            list.add(substr);
+            return;
+        }
         if(open > 0){
-            generateOneByOne(sublist + "(" , list, open-1, close);
+            generateOneByOne(substr + "(" , list, open-1, close);
         }
         if(close > 0){
-            generateOneByOne(sublist + ")" , list, open, close-1);
-        }
-        if(open == 0 && close == 0){
-            list.add(sublist);
-            return;
+            generateOneByOne(substr + ")" , list, open, close-1);
         }
     }
 }
