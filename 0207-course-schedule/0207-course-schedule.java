9e3a318a039ebf20ad6736a1c0bb6cdb824623c3
Anshita Makode <53944279+anshitamakode@users.noreply.github.com>
2025-02-02 15:08:56 +0530

    Time: 6 ms (74.24%), Space: 45.2 MB (76.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<Integer> adjList[] = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
         for(int i=0;i<numCourses;i++){
             adjList[i] = new ArrayList<>();
         }
@@ -8,15 +9,41 @@ class Solution {
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
             adjList[u].add(v);
+            indegree[v]++;
         }
-        int[] vis = new int[numCourses];
+        //DFS
+
+        /*int[] vis = new int[numCourses];
         int[] path = new int[numCourses];
         for(int i=0;i<numCourses;i++){
             if(vis[i] == 0){
                 if(dfs(adjList, vis, path, i) == true) return false;
             }
         }
-        return true;
+        return true;*/
+
+        //BFS
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i] == 0)
+                queue.add(i);
+        }
+        int cnt=0;
+        while(!queue.isEmpty()){
+            int node = queue.remove();
+            cnt++;
+            for(int i=0;i<adjList[node].size();i++){
+                int adjNode = adjList[node].get(i);
+                indegree[adjNode]--;
+                if(indegree[adjNode] == 0)
+                    queue.add(adjNode);
+            }
+        }
+        if(cnt == numCourses)
+            return true;
+        else
+            return false;
     }
     public boolean dfs(List<Integer>[] adjList, int[] vis, int[] path, int i){
         vis[i] = 1;
