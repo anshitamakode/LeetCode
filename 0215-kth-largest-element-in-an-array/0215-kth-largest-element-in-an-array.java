b8eb6da09c7c51fd2be16718176921c285fde0ff
Anshita Makode <53944279+anshitamakode@users.noreply.github.com>
2025-02-16 11:13:36 +0530

    Time: 74 ms (12.97%), Space: 57 MB (93.38%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,43 +1,13 @@
 class Solution {
-    public int partition(int l, int r, int[] nums){
-        int p = l;
-        l = l+1;
-        while(l <= r){
-            if(nums[l] > nums[p] && nums[r] < nums[p]){
-                int temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
-                l++;
-                r--;
-            }
-            if(nums[l] <= nums[p]){
-                l++;
-            }
-            if(nums[r] >= nums[p]){
-                r--;
-            }
-        }
-        int temp = nums[r];
-        nums[r] = nums[p];
-        nums[p] = temp;
-        return r;
-    }
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        int l = 0, r = n - 1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int x : nums){
+            pq.add(x);
+        }
         int ans = 0;
-        while(true){
-            int p = partition(l, r, nums);
-            if(p == n-k){
-                ans = nums[p];
-                break;
-            }
-            else if(p > n-k){
-                r = p-1;
-            }
-            else{
-                l = p+1;
-            }
+        while(k > 0){
+            ans = pq.remove();
+            k--;
         }
         return ans;
     }
